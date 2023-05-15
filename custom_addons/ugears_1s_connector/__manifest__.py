8b8cc29bfd0f786747f8187b04ef1738d3b5d802
bakum.v@gmail.com <bakum.v@gmail.com>
2023-05-15 18:21:19 +0300

    first

--- custom_addons/ugears_1s_connector/__manifest__.py
+++ custom_addons/ugears_1s_connector/__manifest__.py
@@ -12,6 +12,7 @@
 
     'author': "Bakum Viacheslav",
     'website': "https://www.yourcompany.com",
+    'license': "LGPL-3",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
